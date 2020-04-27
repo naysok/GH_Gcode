a7dc92b3315c2a6dfcbd17946073c7d076186405
naysok <ysok.ysok@gmail.com>
2020-04-28 00:44:33 +0900

    [update] add z-offset, fix discontinuity style

--- gh_gcode/operate_curve.py
+++ gh_gcode/operate_curve.py
@@ -1,4 +1,10 @@
+import Rhino.Geometry as rg
 import rhinoscriptsyntax as rs
+
+
+from . import transform
+tr = transform.Transform()
+reload(transform)
 
 
 class Curve():
@@ -13,8 +19,9 @@ class Curve():
         points.append(start_pt)
 
 
-        ### Segment Points / Style : 4 = G1
-        segments = rs.CurveDiscontinuity(polyline, 4)
+        ### Segment Points
+        ### Style : 3 = C2 - Continuous first and second derivative
+        segments = rs.CurveDiscontinuity(polyline, 3)
         for j in range(len(segments)):
             points.append(segments[j])
 
@@ -37,3 +44,22 @@ class Curve():
 
         return points_array
     
+
+    def offset_z(self, points, z_offset_value):
+
+        move_vec = [0, 0, z_offset_value]
+
+        points_off = []
+
+        for i in xrange(len(points)):
+            sub = []
+            tmp = points[i]
+            for j in xrange(len(tmp)):
+                pt = tmp[j]
+                ### offset
+                X, Y, Z = tr.move_pt_vec(pt, move_vec)
+                pt_off = rg.Point3d(X, Y, Z)
+                sub.append(pt_off)
+            points_off.append(sub)
+        
+        return points_off
