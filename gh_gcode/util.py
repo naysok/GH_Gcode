47675b52bc77f0e4ad3c0fb7530a64434e628e7f
naysok_win10 <ysok.ysok@gmail.com>
2020-06-26 22:29:41 +0900

    [update] check print area

--- gh_gcode/util.py
+++ gh_gcode/util.py
@@ -91,6 +91,45 @@ class Util():
         return new_list
     
 
+    def calc_print_area(self, points_array):
+
+        pts = self.flatten_runtime_list(points_array)
+        bbox = rg.BoundingBox(pts)
+        
+        return bbox.Min, bbox.Max
+
+
+    def check_print_area(self, points_array):
+
+        machine_min = [0, 0, 0]
+        machine_max = [1700, 1300, 1020]
+        
+        print_min, print_max = self.calc_print_area(points_array)
+
+        msg = []
+
+        ### Left
+        if machine_min[0] > print_min[0]:
+            msg.append("Over X_Mini!!\n")
+
+        ### Right
+        if machine_max[0] < print_max[0]:
+            msg.append("Over X_Max!!\n")
+
+        ### Front
+        if machine_min[1] > print_min[1]:
+            msg.append("Over Y_Mini!!\n")
+
+        ### Back
+        if machine_max[1] < print_max[1]:
+            msg.append("Over Y_Max!!\n")
+
+        if msg == []:
+            msg.append("OK!!")
+
+        return msg
+
+
     def draw_machine(self):
 
         rects = []
