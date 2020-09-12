a28238fa1137f36ac939082f1d96558f88fa349a
naysok_win10 <ysok.ysok@gmail.com>
2020-09-13 03:21:37 +0900

    [add] viewer_marlin

---
+++ gh/release/cmp_viewer_marlin.py
@@ -0,0 +1,312 @@
+import rhinoscriptsyntax as rs
+import Rhino.Geometry as rg
+
+import datetime
+import math
+import time
+
+
+class Util():
+
+
+    def get_current_time(self):
+        return str(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
+
+
+    def remap_number(self, src, old_min, old_max, new_min, new_max):
+        return ((src - old_min) / (old_max - old_min) * (new_max - new_min) + new_min)
+
+
+    def flatten_runtime_list(self, list):
+        all = []
+        
+        for i in xrange(len(list)):
+            # print(i)
+            sub = list[i]
+            for j in xrange(len(sub)):
+                # print(j)
+                all.append(sub[j])
+
+        return all
+
+
+    def export_gcode(self, dir_path, txt):
+
+        now = self.get_current_time()
+        file_path = dir_path + now + ".gcode"
+
+        ### Export
+        with open(file_path, 'w') as f:
+            f.write(txt)
+
+        print("Export GCode : {}".format(file_path))
+
+
+    def zip_matrix(self, mat):
+        ### https://note.nkmk.me/python-list-transpose/
+        return [list(x) for x in zip(*mat)]
+
+
+    def padding_previous_value(self, list_):
+        
+        list_pad = []
+
+        for i in xrange(len(list_)):
+            
+            item_ = list_[i]
+
+            ### First 
+            if i == 0:
+                if (item_ == None):
+                    list_pad.append(0)
+                else:
+                    list_pad.append(item_)
+            
+            ### Not Frist
+            else:
+                if (item_ == None):
+                    tmp = list_pad[i-1]
+                    list_pad.append(tmp)
+                else:
+                    list_pad.append(item_)
+        
+        return list_pad
+
+
+    def remove_previous_elements(self, a_list):
+
+        ### Remove Same Element as the Previous One
+        new_list = []
+        src_length = len(a_list)
+
+        for i in range(src_length):
+            tmp = a_list[i]
+
+            ### 
+            if i < src_length-1:
+                if a_list[i] != a_list[i+1]:
+                    new_list.append(tmp)
+            ### Last
+            else:
+                new_list.append(tmp)
+                
+        return new_list
+
+ut = Util()
+
+
+class ViewerMarlin():
+    
+    
+    def open_file(self, path):
+        with open(path) as f:
+            l = f.readlines()
+        # print(type(l))
+        # print(len(l))
+        # print(l)
+        return l
+
+
+    def get_value_move(self, str_):
+        
+        ### Split Elements
+        ### Remove n
+        str_.replace("\n", "")
+        
+        ### Remove Comments
+        if ";" in str_:
+            str_rm_comment = str_.split(";")
+            new_str = str_rm_comment[0]
+        else:
+            new_str = str_
+
+        ### Gcode (per Line)
+        # print(new_str)
+
+        ### Sprit Space
+        elements = new_str.split()
+
+        ### init
+        xx = None
+        yy = None
+        zz = None
+        ee = None
+        
+        for i in xrange(len(elements)):
+            elm = elements[i]
+
+            ### Get Value
+            if ("X" in elm):
+                tmp_x = elm.split("X")
+                xx = float(tmp_x[1])
+            
+            elif ("Y" in elm):
+                tmp_y = elm.split("Y")
+                yy = float(tmp_y[1])
+            
+            elif ("Z" in elm):
+                tmp_z = elm.split("Z")
+                zz = float(tmp_z[1])
+
+            elif ("E" in elm):
+                tmp_e = elm.split("E")
+                ee = float(tmp_e[1])
+
+        return [xx, yy, zz, ee]
+
+
+    def gcode_operate_move(self, gcode_line):
+
+        none_list = [None, None, None, None]
+
+        ### Move
+        if ("G0" in gcode_line) or \
+            ("G1" in gcode_line) or \
+            ("G00" in gcode_line) or \
+            ("G01" in gcode_line) or \
+            ("G92 E0" in gcode_line):
+            ### get position
+            return self.get_value_move(gcode_line)
+
+        ### Commment Out
+        elif (";" in gcode_line[0]) or (gcode_line == "\n"):
+            return none_list
+
+        ### Setting G
+        elif ("G4" in gcode_line) or \
+            ("G04" in gcode_line) or \
+            ("G21" in gcode_line) or \
+            ("G28" in gcode_line) or \
+            ("G90" in gcode_line) or \
+            ("G91" in gcode_line) or \
+            ("G92" in gcode_line):
+            
+            return none_list
+
+        ### Setting M
+        elif ("M82" in gcode_line) or \
+            ("M84" in gcode_line) or \
+            ("M104" in gcode_line) or \
+            ("M106" in gcode_line) or \
+            ("M107" in gcode_line) or \
+            ("M109" in gcode_line) or \
+            ("M140" in gcode_line) or \
+            ("M190" in gcode_line) or \
+            ("M204" in gcode_line) or \
+            ("M205" in gcode_line):
+
+            return none_list
+        
+        ### Setting T
+        elif ("T0" in gcode_line) or \
+            ("T1" in gcode_line):
+            return none_list
+
+        else:
+            # return none_list
+            return "bug!"
+
+
+    def gcode_to_array(self, path):
+
+        ### open gcode
+        gcode = self.open_file(path)
+
+        ### Get Vaules from gcode
+        values = []
+        for i in xrange(len(gcode)):
+            gcode_line = gcode[i]
+
+            ### XYZE
+            elements = self.gcode_operate_move(gcode_line)
+
+            ## DEBUG ALL
+            # print(i, gcode_line)
+            ### DEBUG bug
+            if (elements == "bug!"):
+                print(i, gcode_line)
+            ## DEBUG
+
+            values.append(elements)
+
+        ### Padding Previous Value(None)
+        values_zip = ut.zip_matrix(values)
+        # print(len(values_zip))
+        new_values = []
+        for j in xrange(len(values_zip)):
+            list_ = values_zip[j]
+            list_pad = ut.padding_previous_value(list_)
+            new_values.append(list_pad)
+        gcode_values = ut.zip_matrix(new_values)
+
+        # print(len(values))
+        # print(len(gcode_values), len(gcode_values[0]))
+
+        return gcode_values
+
+
+    def draw_path(self, values_4):
+
+        ### Remove Same Element as the Previous One
+        xyze = ut.remove_previous_elements(values_4)
+        ### print(len(values_4), len(xyze))
+
+        ###
+        ### Segment Print / Travel
+        ###
+
+        ### Draw All Path
+        pts = []
+        for i in xrange(len(xyze)):
+            x, y, z, e = values_4[i]
+            pt = [x, y, z]
+            pts.append(pt)
+        
+        return pts
+
+vm = ViewerMarlin()
+
+
+###################################
+
+###################################
+
+
+ghenv.Component.Message = 'Viewer_Marlin / 200821'
+
+
+
+time_0 = time.time()
+
+
+### gcode-file to Memory
+xyze = vm.gcode_to_array(PATH)
+
+
+time_1 = time.time()
+
+
+### Pick Values
+xyz = vm.draw_path(xyze)
+
+
+
+time_2 = time.time()
+
+
+### Draw Printing
+MOVE = rs.AddPolyline(xyz)
+
+
+time_3 = time.time()
+
+
+
+
+#time_01 = time_1 - time_0
+#time_12 = time_2 - time_1
+#time_23 = time_3 - time_2
+#
+#print ("Time 01 : {0}".format(time_01) + "[sec]")
+#print ("Time 12 : {0}".format(time_12) + "[sec]")
+#print ("Time 23 : {0}".format(time_23) + "[sec]")
