371ae168f644ca497c917253a042352a69011452
naysok_win10 <ysok.ysok@gmail.com>
2020-10-10 18:55:04 +0900

    [add] render points weight

---
+++ gh/release/render_points_weight.py
@@ -0,0 +1,258 @@
+import rhinoscriptsyntax as rs
+import Rhino.Geometry as rg
+
+import datetime
+import math
+
+
+class Util():
+
+
+    def get_current_time(self):
+        return str(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
+
+
+    def remap_number(self, src, old_min, old_max, new_min, new_max):
+        return ((src - old_min) / (old_max - old_min) * (new_max - new_min) + new_min)
+
+
+    def flatten_runtime_list(self, list):
+        all = []
+        
+        for i in xrange(len(list)):
+            # print(i)
+            sub = list[i]
+            for j in xrange(len(sub)):
+                # print(j)
+                all.append(sub[j])
+
+        return all
+
+
+    def export_gcode(self, dir_path, txt):
+
+        now = self.get_current_time()
+        file_path = dir_path + now + ".gcode"
+
+        ### Export
+        with open(file_path, 'w') as f:
+            f.write(txt)
+
+        print("Export GCode : {}".format(file_path))
+
+
+    def zip_matrix(self, mat):
+        ### https://note.nkmk.me/python-list-transpose/
+        return [list(x) for x in zip(*mat)]
+
+
+    def padding_previous_value(self, list_):
+        
+        list_pad = []
+
+        for i in xrange(len(list_)):
+            
+            item_ = list_[i]
+
+            ### First 
+            if i == 0:
+                if (item_ == None):
+                    list_pad.append(0)
+                else:
+                    list_pad.append(item_)
+            
+            ### Not Frist
+            else:
+                if (item_ == None):
+                    tmp = list_pad[i-1]
+                    list_pad.append(tmp)
+                else:
+                    list_pad.append(item_)
+        
+        return list_pad
+
+
+    def remove_previous_elements(self, a_list):
+
+        ### Remove Same Element as the Previous One
+        new_list = []
+        src_length = len(a_list)
+
+        for i in range(src_length):
+            tmp = a_list[i]
+
+            ### 
+            if i < src_length-1:
+                if a_list[i] != a_list[i+1]:
+                    new_list.append(tmp)
+            ### Last
+            else:
+                new_list.append(tmp)
+                
+        return new_list
+
+
+    def bitwise_or_arrays(self, arrays):
+
+        ### Merge Bool from Brep.isPointInside
+
+        # print(len(arrays[0]))
+        # print(len(arrays[0][0]))
+
+        if len(arrays) == 1:
+            return arrays[0]
+
+        else:
+            arrays_zip = self.zip_matrix(arrays)
+            # print(len(arrays_zip))
+            # print(len(arrays_zip[0]))
+            # print(len(arrays_zip[0][0]))
+
+            bool_inside = []
+
+            for i in xrange(len(arrays_zip)):
+                # print(i)
+
+                sub_ = []
+
+                item = arrays_zip[i]
+                item_zip = self.zip_matrix(item)
+
+                # print(len(item_zip))
+                # print(len(item_zip[0]))
+
+                for j in xrange(len(item_zip)):
+
+                    values = item_zip[j]
+                    
+                    if True in values:
+                        sub_.append(True)
+                    else:
+                        sub_.append(False)
+
+                bool_inside.append(sub_)
+
+            # print(len(bool_inside))
+            # print(len(bool_inside[0]))
+
+            return bool_inside
+
+
+### Utilities fo EB
+class Util_EB(Util):
+
+
+    def calc_print_area(self, points_array):
+
+        pts = self.flatten_runtime_list(points_array)
+        bbox = rg.BoundingBox(pts)
+        
+        return bbox.Min, bbox.Max
+
+
+    def check_print_area(self, points_array):
+
+        machine_min = [0, 0, 0]
+        machine_max = [1700, 1300, 1020]
+        
+        print_min, print_max = self.calc_print_area(points_array)
+
+        bbox = None
+        msg = []
+
+        ### Draw Bouinding Box
+        bbox = rg.BoundingBox(print_min, print_max)
+
+        ### Check Print Area
+        ### Left
+        if machine_min[0] > print_min[0]:
+            msg.append("Over X_Mini!!\n")
+
+        ### Right
+        if machine_max[0] < print_max[0]:
+            msg.append("Over X_Max!!\n")
+
+        ### Front
+        if machine_min[1] > print_min[1]:
+            msg.append("Over Y_Mini!!\n")
+
+        ### Back
+        if machine_max[1] < print_max[1]:
+            msg.append("Over Y_Max!!\n")
+        
+        ### Bottom
+        if machine_min[2] > print_min[2]:
+            msg.append("Over Z_Mini!!\n")
+
+        if msg == []:
+            msg.append("OK!!")
+
+        return msg, bbox
+
+
+    def draw_machine(self):
+
+        rects = []
+
+        machine_origin = [-69, -100]
+        machine_max = [1700, 1300]
+
+        ### Panel
+        panel_size = 500
+        panel_count = 12
+        vv = 3
+
+        for i in xrange(panel_count):
+
+            u, v = divmod(i, vv)
+            tmp_origin = (machine_origin[0] + (500 * u) , machine_origin[1] + (500 *v), 0)
+            pl = rs.MovePlane(rs.WorldXYPlane(), tmp_origin)
+            rc = rs.AddRectangle(pl, panel_size, panel_size)
+            rects.append(rc)
+
+        ### Print Area
+        print_origin = (0, 0, 0)
+        pl = rs.MovePlane(rs.WorldXYPlane(), print_origin)
+        rc = rs.AddRectangle(pl, machine_max[0], machine_max[1])
+        rects.append(rc)
+
+        return rects
+
+
+    def print_preview(self, points_array):
+
+        pls = []
+        lens_ = 0.0
+
+        for i in xrange(len(points_array)):
+            pl = rg.Polyline(points_array[i])
+            pls.append(pl)
+            
+            len_ = pl.Length
+            lens_ += float(len_)
+        
+        lens_int = int(lens_)
+
+        return pls, lens_int
+
+
+
+ut = Util_EB()
+
+
+
+###########
+
+
+ghenv.Component.Message = 'Render Points + Weight / 201010'
+
+
+if __DRAW:
+    __DRAW_POINTS = ut.flatten_runtime_list(POINTS)    
+    ww = ut.flatten_runtime_list(WEIGHT)
+    
+    www = []
+    for i in xrange(len(ww)):
+        www.append(rs.CreateColor(0, 0, ww[i] * 255))
+    
+    __DRAW_WEIGHT = www
